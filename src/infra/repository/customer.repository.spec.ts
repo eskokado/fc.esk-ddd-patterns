0309a792d1c7237cbf44a7fdbb8dca9bbefd25c2
eskokado <eskokado@gmail.com>
2022-03-19 16:53:03 -0300

    test: find and findAll customer

--- src/infra/repository/customer.repository.spec.ts
+++ src/infra/repository/customer.repository.spec.ts
@@ -67,4 +67,35 @@ describe("Customer repository test", () => {
     });
   });
 
+  it("should throw an error when customer is not found", async () => {
+    const customerRepository = new CustomerRepository();
+
+    expect(async () => {
+      await customerRepository.find("456ABC");
+    }).rejects.toThrow("Customer not found");
+  });
+
+  it("should find all customers", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer1 = new Customer("123", "Customer 1");
+    const address1 = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer1.Address = address1;
+    customer1.addRewardPoints(10);
+    customer1.activate();
+
+    const customer2 = new Customer("456", "Customer 2");
+    const address2 = new Address("Street 2", 2, "Zipcode 2", "City 2");
+    customer2.Address = address2;
+    customer2.addRewardPoints(20);
+
+    await customerRepository.create(customer1);
+    await customerRepository.create(customer2);
+
+    const customers = await customerRepository.findAll();
+
+    expect(customers).toHaveLength(2);
+    expect(customers).toContainEqual(customer1);
+    expect(customers).toContainEqual(customer2);
+  });
+
 })
