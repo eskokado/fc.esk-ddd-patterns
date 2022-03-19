5e95ca60aa0990fe85de5a02dd1c30fb11e6dc57
eskokado <eskokado@gmail.com>
2022-03-19 08:39:38 -0300

    feat: autovalidacoes

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,13 +1,13 @@
 class Customer {
   private _id: string;
-  private _name: string;
-  private _address: string;
-  private _active: boolean = true;
+  private _name: string = "";
+  private _address: string = "";
+  private _active: boolean = false;
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
+    this.validate();
   }
 
   get id(): string {
@@ -22,11 +22,24 @@ class Customer {
     return this._address;
   }
 
-  changeName(name: string) {
-    this._name = name;
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
   }
 
-  activate() {
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  activate() {        
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
     this._active = true;
   }
 
