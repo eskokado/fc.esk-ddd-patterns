2c06d39e4559dfc3605cdaf53cc71fec71e4cd3b
eskokado <eskokado@gmail.com>
2022-03-19 16:42:57 -0300

    feat: update customer

--- src/infra/repository/customer.repository.ts
+++ src/infra/repository/customer.repository.ts
@@ -16,9 +16,26 @@ export default class CustomerRepository implements CustomerRepositoryInterface {
     });
   }
   
-  update(entity: Customer): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async update(entity: Customer): Promise<void> {
+    await CustomerModel.update(
+      {
+        name: entity.name,
+        street: entity.Address.street,
+        number: entity.Address.number,
+        zipcode: entity.Address.zip,
+        city: entity.Address.city,
+        active: entity.isActive(),
+        rewardPoints: entity.rewardPoints,
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
   }
+  
   find(id: string): Promise<Customer> {
     throw new Error("Method not implemented.");
   }
