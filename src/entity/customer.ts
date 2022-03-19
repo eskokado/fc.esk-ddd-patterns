3431cf41f27e0a161e48832b2ee6d01cb18f4352
eskokado <eskokado@gmail.com>
2022-03-19 09:10:08 -0300

    feat: value object

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,7 +1,9 @@
+import Address from "./address";
+
 class Customer {
   private _id: string;
   private _name: string = "";
-  private _address: string = "";
+  private _address!: Address;
   private _active: boolean = false;
 
   constructor(id: string, name: string) {
@@ -18,10 +20,6 @@ class Customer {
     return this._name;
   }
 
-  get address(): string {
-    return this._address;
-  }
-
   validate() {
     if (this._id.length === 0) {
       throw new Error("Id is required");
@@ -36,14 +34,19 @@ class Customer {
     this.validate();
   }
 
-  activate() {        
-    if (this._address.length === 0) {
+  activate() {         
+    if (this._address === undefined) {
       throw new Error("Address is mandatory to activate a customer");
     }
+    
     this._active = true;
   }
 
   deactivate() {
     this._active = false;
+  }
+
+  set Address(address: Address) {
+      this._address = address;
   }
 }
