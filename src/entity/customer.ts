a59246f12da942fd44d3fa490bb22da2d410faaa
eskokado <eskokado@gmail.com>
2022-03-19 08:09:21 -0300

    feat: criando entidade anemica

---
+++ src/entity/customer.ts
@@ -0,0 +1,23 @@
+class Customer {
+  private _id: string;
+  private _name: string;
+  private _address: string;
+
+  constructor(id: string, name: string, address: string) {
+    this._id = id;
+    this._name = name;
+    this._address = address;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get address(): string {
+    return this._address;
+  }
+}
