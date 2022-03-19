5b8773dc895f4dd74867ae4500ee655c6c35c0e4
eskokado <eskokado@gmail.com>
2022-03-19 15:59:27 -0300

    test: findOne and findAll product

--- src/infra/repository/product.repository.spec.ts
+++ src/infra/repository/product.repository.spec.ts
@@ -64,5 +64,37 @@ describe("Product repository test", () => {
       name: "Product 2",
       price: 200,
     });
-  });  
+  });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+
+    await productRepository.create(product);
+
+    const productModel = await ProductModel.findOne({ where: { id: "1" } });
+
+    const foundProduct = await productRepository.find("1");
+
+    expect(productModel.toJSON()).toStrictEqual({
+      id: foundProduct.id,
+      name: foundProduct.name,
+      price: foundProduct.price,
+    });
+  });    
+
+
+  it("should find all products", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+    await productRepository.create(product);
+
+    const product2 = new Product("2", "Product 2", 200);
+    await productRepository.create(product2);
+
+    const foundProducts = await productRepository.findAll();
+    const products = [product, product2];
+
+    expect(products).toEqual(foundProducts);    
+  });
 });
