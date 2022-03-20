b0e1380459da66a69eb5d5ea78a9751d844f6bd4
eskokado <eskokado@gmail.com>
2022-03-19 22:45:08 -0300

    feat: update order

--- src/infra/repository/order.repository.ts
+++ src/infra/repository/order.repository.ts
@@ -1,9 +1,10 @@
 import Order from "../../domain/entity/order";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
-export default class OrderRepository {
-    async create(entity: Order): Promise<void> {
+export default class OrderRepository implements OrderRepositoryInterface {
+  async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
         id: entity.id,
@@ -22,4 +23,52 @@ export default class OrderRepository {
       }
     );
   }
+
+  async update(entity: Order): Promise<void> {
+    let orderModel;
+    var id = entity.id;
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        rejectOnEmpty: true,
+      });
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+
+    id = entity.id;
+    await OrderModel.destroy({
+      where: { id },
+    });
+    
+    await OrderModel.create(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        include: [{ model: OrderItemModel }],
+      }
+    );
+  }
+
+  find(id: string): Promise<Order> {
+    throw new Error("Method not implemented.");
+  }
+
+  findAll(): Promise<Order[]> {
+    throw new Error("Method not implemented.");
+  }
+
+
 }
