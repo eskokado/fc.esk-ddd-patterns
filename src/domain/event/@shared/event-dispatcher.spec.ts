06adafeba45f9905e8f8fdf26ae43db653ae5e73
eskokado <eskokado@gmail.com>
2022-03-29 04:56:29 -0300

    test: ensure unregister an event handler

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -10,5 +10,18 @@ describe("Domain events tests", () => {
 
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+  });
+
+  it("Should unregister an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+    eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0);
   });
 });
