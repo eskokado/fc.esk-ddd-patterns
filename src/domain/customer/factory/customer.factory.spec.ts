53b9a35e2b4e9b2269367e126b7f4cea83546ba0
eskokado <eskokado@gmail.com>
2022-04-01 06:18:26 -0300

    test: create customer without and with address wiht factory

---
+++ src/domain/customer/factory/customer.factory.spec.ts
@@ -0,0 +1,26 @@
+import Address from "../value-object/address";
+import CustomerFactory from "./customer.factory";
+
+describe("Customer factory unit test", () => {
+  it("should create a customer", () => {
+    let customer = CustomerFactory.create("John");
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("John");
+    expect(customer.Address).toBeUndefined();
+  });
+
+  it("should create a customer with an address", () => {
+    const address = new Address("Endereco 1", 15, "12345678", "São Paulo");
+    let customer = CustomerFactory.createWithAddress("John", address);
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("John");
+    expect(customer.Address).toBe(address);
+  })
+
+  it("should throw when create customer with name invalid", () => {
+    expect(() => CustomerFactory.create(""))
+      .toThrowError("Customer not created");
+  }); 
+});
