25e383d135008adaf8076572c84bd9b0564ba35a
eskokado <eskokado@gmail.com>
2022-04-30 17:12:31 -0300

    test: ensure success when call update product use case with valids datas

---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,29 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import UpdateProductUseCase from "./update.product.usecase";
+
+const product = ProductFactory.create("a", "Product 1", 10);
+
+const MockRepository = () => {
+  return {
+    findAll: jest.fn(),
+    create: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    update: jest.fn(),
+  }
+}
+
+describe("Unit Test update product use case", () => {
+  it("should update a product", async () => {
+    const input = {
+      id: product.id,
+      name: "Product update",
+      price: 20
+    }
+    const productRepository = MockRepository();
+    const updateProductUseCase = new UpdateProductUseCase(productRepository);
+
+    const output = await updateProductUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
