5cd4d5fb9e47c15824f6af8b477ba347d6d64466
eskokado <eskokado@gmail.com>
2022-04-30 17:38:43 -0300

    test: ensure returns list product when call list product usecase

---
+++ src/usecase/product/list/list.product.unit.spec.ts
@@ -0,0 +1,39 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ListProductUseCase from "./list.product.usecase";
+
+const product1 = ProductFactory.create("a", "Product 1", 10);
+const product2 = ProductFactory.create("a", "Product 2", 20);
+
+const input = [
+  product1,
+  product2
+]
+
+const MockRepository = () => {
+  return {
+    findAll: jest.fn().mockReturnValue(Promise.resolve(input)),
+    create: jest.fn(),
+    find: jest.fn(),
+    update: jest.fn(),
+  }
+}
+
+describe("Unit Test for listing product use case", () => {
+  it("should list a products", async () => {
+    const productRepository = MockRepository();
+    const listProductUseCase = new ListProductUseCase(productRepository);
+
+    const output = await listProductUseCase.execute({});
+
+    expect(output.products.length).toBe(2);
+
+    expect(output.products[0].id).toBe(product1.id);
+    expect(output.products[0].name).toBe(product1.name);
+    expect(output.products[0].price).toBe(product1.price);
+
+    expect(output.products[1].id).toBe(product2.id);
+    expect(output.products[1].name).toBe(product2.name);
+    expect(output.products[1].price).toBe(product2.price);
+  });
+});
+
