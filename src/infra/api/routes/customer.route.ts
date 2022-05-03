f9326ce840fe457009efc228fe039da099d635ea
eskokado <eskokado@gmail.com>
2022-05-03 18:33:40 -0300

    feat: create customer for e2e

---
+++ src/infra/api/routes/customer.route.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
+import CustomerRepository from "../../customer/repository/customer.repository";
+
+export const customerRoute = express.Router();
+
+customerRoute.post("/", async (req: Request, res: Response) => {
+  const usecase = new CreateCustomerUseCase(new CustomerRepository());
+  try {
+    const input = {
+      name: req.body.name,
+      Address: {
+        street: req.body.Address.street,
+        city: req.body.Address.city,
+        number: req.body.Address.number,
+        zip: req.body.Address.zip
+      } 
+    }
+
+    const output = await usecase.execute(input);
+
+    res.status(201).send(output);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
