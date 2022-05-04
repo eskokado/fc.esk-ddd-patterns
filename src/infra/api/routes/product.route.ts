a9fe81d5ac29bd9dd81d95fab92390aff9c2d563
eskokado <eskokado@gmail.com>
2022-05-04 17:04:41 -0300

    feat: get list all products

--- src/infra/api/routes/product.route.ts
+++ src/infra/api/routes/product.route.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from "express";
 import CreateProductUseCase from "../../../usecase/product/create/create.product.usecase";
+import ListProductUseCase from "../../../usecase/product/list/list.product.usecase";
 import ProductRepository from "../../product/repository/product.repository";
 
 export const productRoute = express.Router();
@@ -15,4 +16,14 @@ productRoute.post("/", async (req: Request, res: Response) => {
   const output = await usecase.execute(input);
 
   res.status(201).send(output);  
+});
+
+productRoute.get("/", async (req: Request, res: Response) => {
+  const usecase = new ListProductUseCase(new ProductRepository());
+  try {
+    const output = await usecase.execute({});
+    res.send(output);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
