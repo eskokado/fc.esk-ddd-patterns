914a57775d95aa03edbd014cf4e4492b7d91c7b4
eskokado <eskokado@gmail.com>
2022-05-06 18:32:25 -0300

    test: notification when filter context

---
+++ src/domain/@shared/notification/notification.spec.ts
@@ -0,0 +1,28 @@
+import Notification from "./notification";
+
+describe("Unit tests for notifications", () => {
+
+  it("should create errors", () => {
+
+    const notification = new Notification();
+    const error = {
+      message: "error message",
+      context: "customer"
+    }
+
+    notification.addError(error);
+
+    expect(notification.messages("customer")).toBe("customer: error message,");
+
+    const error2 = {
+      message: "error message2",
+      context: "customer"
+    }
+
+    notification.addError(error2);
+
+    expect(notification.messages("customer")).toBe("customer: error message,customer: error message2,");
+
+  });
+
+});
