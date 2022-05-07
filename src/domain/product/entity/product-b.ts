c207c73051b73c3ede09005138fc715e51b58044
eskokado <eskokado@gmail.com>
2022-05-07 16:21:37 -0300

    feat: notification errors

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,11 +1,13 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 import ProductInterface from "./product.interface";
 
-export default class ProductB implements ProductInterface{
-  private _id: string;
+export default class ProductB extends Entity implements ProductInterface{
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
@@ -32,6 +34,12 @@ export default class ProductB implements ProductInterface{
   changePrice(price: number): void {
     this._price = price;
     this.validate();
+  }
+
+  checkErrors() {
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   validate(): boolean {
--- src/domain/product/entity/product.interface.ts
+++ src/domain/product/entity/product.interface.ts
@@ -2,6 +2,7 @@ export default interface ProductInterface {
   get id(): string;
   get name(): string;
   get price(): number;
+  checkErrors(): void;
   changeName(name: string): void;
   changePrice(price: number): void;
 }
