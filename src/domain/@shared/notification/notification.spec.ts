4cf1f6f4888bbfbd548d741131325006931f654b
eskokado <eskokado@gmail.com>
2022-05-07 10:36:08 -0300

    fix: changed message

--- src/domain/@shared/notification/notification.spec.ts
+++ src/domain/@shared/notification/notification.spec.ts
@@ -12,7 +12,7 @@ describe("Unit tests for notifications", () => {
 
     notification.addError(error);
 
-    expect(notification.messages("customer")).toBe("customer: error message,");
+    expect(notification.messages("customer")).toBe("customer: error message");
 
     const error2 = {
       message: "error message2",
@@ -21,7 +21,7 @@ describe("Unit tests for notifications", () => {
 
     notification.addError(error2);
 
-    expect(notification.messages("customer")).toBe("customer: error message,customer: error message2,");
+    expect(notification.messages("customer")).toBe("customer: error message, customer: error message2");
 
     const error3 = {
       message: "error message3",
@@ -30,9 +30,8 @@ describe("Unit tests for notifications", () => {
 
     notification.addError(error3);
 
-    expect(notification.messages("customer")).toBe("customer: error message,customer: error message2,");
-    expect(notification.messages()).toBe("customer: error message,customer: error message2,order: error message3,");
-
+    expect(notification.messages("customer")).toBe("customer: error message, customer: error message2");
+    expect(notification.messages()).toBe("customer: error message, customer: error message2, order: error message3");
   });
 
   it("should check if notification has at least one error", () => {
